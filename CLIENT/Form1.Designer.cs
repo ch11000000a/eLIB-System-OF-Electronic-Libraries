ce527b922200d00144c07a282c4efd685f51cd70
diezel <diezel@xakep.ru>
2012-10-22 01:11:38 +0600

    update

---
+++ CLIENT/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace elib
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.авторизацияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.поискToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.всеКнигиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.всеТекстаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.всеРефератыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузкаКнигToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.пользователиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.статистикаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 27);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(995, 563);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("http://localhost/elib", System.UriKind.Absolute);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.настройкиToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(995, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.авторизацияToolStripMenuItem,
+            this.поискToolStripMenuItem,
+            this.всеКнигиToolStripMenuItem,
+            this.всеТекстаToolStripMenuItem,
+            this.всеРефератыToolStripMenuItem,
+            this.загрузкаКнигToolStripMenuItem,
+            this.пользователиToolStripMenuItem,
+            this.статистикаToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(85, 20);
+            this.toolStripMenuItem1.Text = "Управление";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // авторизацияToolStripMenuItem
+            // 
+            this.авторизацияToolStripMenuItem.Name = "авторизацияToolStripMenuItem";
+            this.авторизацияToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.авторизацияToolStripMenuItem.Text = "Главная";
+            this.авторизацияToolStripMenuItem.Click += new System.EventHandler(this.авторизацияToolStripMenuItem_Click);
+            // 
+            // поискToolStripMenuItem
+            // 
+            this.поискToolStripMenuItem.Name = "поискToolStripMenuItem";
+            this.поискToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.поискToolStripMenuItem.Text = "Поиск";
+            this.поискToolStripMenuItem.Click += new System.EventHandler(this.поискToolStripMenuItem_Click);
+            // 
+            // всеКнигиToolStripMenuItem
+            // 
+            this.всеКнигиToolStripMenuItem.Name = "всеКнигиToolStripMenuItem";
+            this.всеКнигиToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.всеКнигиToolStripMenuItem.Text = "Все Книги";
+            this.всеКнигиToolStripMenuItem.Click += new System.EventHandler(this.всеКнигиToolStripMenuItem_Click);
+            // 
+            // всеТекстаToolStripMenuItem
+            // 
+            this.всеТекстаToolStripMenuItem.Name = "всеТекстаToolStripMenuItem";
+            this.всеТекстаToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.всеТекстаToolStripMenuItem.Text = "Все Текста";
+            // 
+            // всеРефератыToolStripMenuItem
+            // 
+            this.всеРефератыToolStripMenuItem.Name = "всеРефератыToolStripMenuItem";
+            this.всеРефератыToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.всеРефератыToolStripMenuItem.Text = "Все Рефераты";
+            // 
+            // загрузкаКнигToolStripMenuItem
+            // 
+            this.загрузкаКнигToolStripMenuItem.Name = "загрузкаКнигToolStripMenuItem";
+            this.загрузкаКнигToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.загрузкаКнигToolStripMenuItem.Text = "Загрузка Книг";
+            this.загрузкаКнигToolStripMenuItem.Click += new System.EventHandler(this.загрузкаКнигToolStripMenuItem_Click);
+            // 
+            // пользователиToolStripMenuItem
+            // 
+            this.пользователиToolStripMenuItem.Name = "пользователиToolStripMenuItem";
+            this.пользователиToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.пользователиToolStripMenuItem.Text = "Пользователи";
+            this.пользователиToolStripMenuItem.Click += new System.EventHandler(this.пользователиToolStripMenuItem_Click);
+            // 
+            // статистикаToolStripMenuItem
+            // 
+            this.статистикаToolStripMenuItem.Name = "статистикаToolStripMenuItem";
+            this.статистикаToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.статистикаToolStripMenuItem.Text = "Статистика";
+            this.статистикаToolStripMenuItem.Click += new System.EventHandler(this.статистикаToolStripMenuItem_Click);
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            this.настройкиToolStripMenuItem.Click += new System.EventHandler(this.настройкиToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(829, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(742, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Прокси сервер:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(655, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Статус:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(699, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 602);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Электронная Библиотека - Client for \"eLIB v. 0.5\" Beta (build 1612)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem авторизацияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem поискToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem всеКнигиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem всеТекстаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem всеРефератыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузкаКнигToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem пользователиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem статистикаToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CLIENT/Form2.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Сериализация
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace elib
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
+            // Изменяем конфиг
+            string temp2_proxy_ip = this.textBox2.Text;
+            Properties.Settings.Default.proxy_ip = temp2_proxy_ip;
+
+            string temp2_proxy_port = this.textBox1.Text;
+            Properties.Settings.Default.proxy_port = temp2_proxy_port;
+
+            // ***************
+            if (radioButton1.Checked == true)
+            {
+                string temp_proxy_on_off = "0";
+                Properties.Settings.Default.proxy_on_off = temp_proxy_on_off;
+            }
+            else
+            {
+                string temp_proxy_on_off = "1";
+                Properties.Settings.Default.proxy_on_off = temp_proxy_on_off;
+            }
+            // ***************
+
+            // Proxy.Set(null); //Отключить прокси
+
+            // Сохраняем конфиг
+            Properties.Settings.Default.Save();
+            Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // Читаем конфиг
+            string temp1_proxy_ip = Properties.Settings.Default.proxy_ip;
+            temp1_proxy_ip = this.textBox2.Text;
+
+            string temp1_proxy_port = Properties.Settings.Default.proxy_port;
+            temp1_proxy_port = this.textBox1.Text;
+             
+            // Выставляем значение радио баттонов
+            string temp_proxy_on_off = Properties.Settings.Default.proxy_on_off;
+            int temp_proxy_on_off_int = Convert.ToInt32(temp_proxy_on_off);
+            if (temp_proxy_on_off_int == 1)
+            {
+                radioButton2.Checked = true;
+            }
+            else
+            {
+                radioButton1.Checked = true;
+            }
+             
+            // скрываем лейблы и тексты если р.б.1 тру
+            if (radioButton1.Checked == true)
+            {
+                label1.Visible = false;
+                label2.Visible = false;
+                textBox2.Visible = false;
+                textBox1.Visible = false;
+            }
+            else
+            {
+                label1.Visible = true;
+                label2.Visible = true;
+                textBox2.Visible = true;
+                textBox1.Visible = true;
+            }
+
+            // устраняем баг с визибл
+            if (button2.Visible == true)
+            {
+                label1.Visible = false;
+                label2.Visible = false;
+                textBox2.Visible = false;
+                textBox1.Visible = false;
+            }
+            return;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            label3.Visible = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            label3.Visible = true;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //разрешить только цифры 
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+                return;
+            //разрешить <Enter> <Beckspace> esc... 
+            if (Char.IsControl(e.KeyChar))
+                return;
+            //все остальное запрещаем
+            e.Handled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // делаем невидимыми лейблы, текстбоксы и кнопку
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            button2.Visible = false;
+
+            // тут будет условие
+            string login = textBox3.Text;
+            string pass = textBox4.Text;
+            string login_true = "admin";
+            string pass_true = "123456";
+
+
+            if (login == login_true && pass == pass_true)
+            {
+                // если логин и пасс тру то визибл настроек = тру
+                button1.Visible = true;
+                label8.Visible = false;
+                radioButton1.Visible = true;
+                radioButton2.Visible = true;
+            }
+            else
+            {
+                label7.Visible = true;
+                label8.Visible = false;
+            }
+            return;
+        }
+
+    
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                label1.Visible = false;
+                label2.Visible = false;
+                textBox2.Visible = false;
+                textBox1.Visible = false;
+            }
+            else
+            {
+                label1.Visible = true;
+                label2.Visible = true;
+                textBox2.Visible = true;
+                textBox1.Visible = true;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                label1.Visible = false;
+                label2.Visible = false;
+                textBox2.Visible = false;
+                textBox1.Visible = false;
+            }
+            else
+            {
+                label1.Visible = true;
+                label2.Visible = true;
+                textBox2.Visible = true;
+                textBox1.Visible = true;
+            }
+        }
+
+       
+    }
+}
