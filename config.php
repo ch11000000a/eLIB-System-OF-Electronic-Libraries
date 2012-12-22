17ff674f5499ce85c51f51a3d6c69c8a20f36550
diezel <diezel@xakep.ru>
2012-12-22 16:05:02 +0600

    upd

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	define ("COPY", "eLIB&nbsp;v.&nbsp;1.0&nbsp;&copy;&nbsp;<a href='mailto:diezel@xakep.ru'>Гонцов&nbsp;Д.</a>&nbsp;2012");
 	define ("VERSION", "eLIB v. 1.0");
 	
-	$db = mysql_connect ("localhost","root","H7jYwRGRes3meBQs");
+	$db = mysql_connect ("host","login","pass");
     mysql_query('SET NAMES cp1251');
     mysql_select_db ("elib",$db);
 ?>
